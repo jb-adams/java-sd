be44e2903c98e730016c416c62d9b4ed89fc5439
Jeremy Adams <jeremybr.adams@gmail.com>
2018-03-27 11:26:13 -0400

    modified HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -3,6 +3,7 @@ package basics;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("I'm adding code to my project");
 
     }
 }
