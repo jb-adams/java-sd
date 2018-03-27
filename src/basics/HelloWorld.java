87d31386b032fb366bd1c3fc708b660757846e4f
Jeremy Adams <jeremybr.adams@gmail.com>
2018-03-27 11:28:11 -0400

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("I'm adding code to my project");
-
+        System.out.println("I'm adding code from github");
     }
 }
